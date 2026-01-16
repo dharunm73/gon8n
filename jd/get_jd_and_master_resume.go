79b5b99ab850c66807a49886344bfeab3c4ed8cc
Dharun <dharunm7733@gmail.com>
2026-01-16 23:51:08 +0530

    simple popup is done

--- jd/get_jd_and_master_resume.go
+++ jd/get_jd_and_master_resume.go
@@ -1,8 +1,10 @@
 package jd
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
 )
 
 var JobDesc string
@@ -10,15 +12,32 @@ var JobDesc string
 var Master string
 
 func GetJD() string {
-	data, err := os.ReadFile("jd.txt")
-	if err != nil {
+
+	fmt.Println("Enter the Job Descritpion in the notepad, if understood, press Enter")
+	fmt.Scanln()
+
+	file, _ := os.OpenFile("jd.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	file.Close()
+
+	opencmd := exec.Command("notepad.exe", "jd.txt")
+	if err := opencmd.Start(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("After saving the file, press Enter here")
+	fmt.Scanln()
+
+	data, _ := os.ReadFile("jd.txt")
+
 	JobDesc = string(data)
+
 	return JobDesc
 }
 
 func MasterResume() string {
+
+	fmt.Println("Reading master resume json")
+
 	data, err := os.ReadFile("master_resume.json")
 	if err != nil {
 		log.Fatal(err)
